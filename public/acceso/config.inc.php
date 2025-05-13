d6a71e780e17542a9d5813e088c4aff1747f17e8
jererosas <62047397+jererosas@users.noreply.github.com>
2025-05-13 03:28:02 -0300

    update

--- public/acceso/config.inc.php
+++ public/acceso/config.inc.php
@@ -16,16 +16,16 @@ $i = 0;
 $i++;
 
 /* Authentication type and info */
-$cfg['Servers'][$i]['auth_type'] = 'config';
-$cfg['Servers'][$i]['user'] = 'root';
-$cfg['Servers'][$i]['password'] = 'Jeremix11';
-$cfg['Servers'][$i]['extension'] = 'mysqli';
-$cfg['Servers'][$i]['AllowNoPassword'] = true;
+$cfg['Servers'][$i]['auth_type'] = 'cookie';
+$cfg['Servers'][$i]['host'] = 'localhost';
+$cfg['Servers'][$i]['connect_type'] = 'tcp';
+$cfg['Servers'][$i]['compress'] = false;
+$cfg['Servers'][$i]['AllowNoPassword'] = false;
 $cfg['Lang'] = '';
 
 /* Bind to the localhost ipv4 address and tcp */
-$cfg['Servers'][$i]['host'] = '127.0.0.1';
-$cfg['Servers'][$i]['connect_type'] = 'tcp';
+$cfg['Servers'][$i]['host'] = 'localhost';
+$cfg['Servers'][$i]['connect_type'] = 'tcp'; 
 
 /* User for advanced features */
 $cfg['Servers'][$i]['controluser'] = 'pma';
